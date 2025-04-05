72fe98cc7c1a6706133857f6c50bc6d0cebf3136
steve <changlin.stevetsai@gmail.com>
2025-04-05 20:30:53 +0800

    DTO

--- src/main/java/com/shop/project/controller/CategoryController.java
+++ src/main/java/com/shop/project/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.shop.project.controller;
 
 
 import com.shop.project.model.Category;
+import com.shop.project.pyaload.CategoryDTO;
 import com.shop.project.pyaload.CategoryResponse;
 import com.shop.project.service.CategoryService;
 import jakarta.validation.Valid;
@@ -30,9 +31,9 @@ public class CategoryController {
 
     @PostMapping("/public/categories")
     //@RequestMapping(value = "/public/categories", method = RequestMethod.POST)
-    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category){
-        categoryService.createCategory(category);
-        return new ResponseEntity<>("Category added successfully", HttpStatus.CREATED);
+    public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO){
+        CategoryDTO savedCategoryDTO = categoryService.createCategory(categoryDTO);
+        return new ResponseEntity<>(savedCategoryDTO, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/admin/categories/{categoryId}")
